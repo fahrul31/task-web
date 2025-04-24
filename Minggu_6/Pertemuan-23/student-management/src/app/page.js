eaa6238c45d0874061faabf05cf0733107f212bb
fahrul31 <fahrulhidayat73@gmail.com>
2025-04-24 22:39:24 +0800

    Update: main page.js to dashboard

--- Minggu_6/Pertemuan-23/student-management/src/app/page.js
+++ Minggu_6/Pertemuan-23/student-management/src/app/page.js
@@ -1,11 +1,10 @@
 import Dashboard from "@/app/dashboard/page";
-import Image from "next/image";
-import LoginPage from "./login/page";
+// import LoginPage from "./login/page";
 
 export default function Home() {
   return (
     <div className="bg-gray-500 h-screen">
-      <LoginPage />
+      <Dashboard />
     </div>
   );
 }
